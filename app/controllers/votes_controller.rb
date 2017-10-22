d34c3f924661d3f5b384fa98663808d1c1e27e8b
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-10-21 18:31:57 -0700

    Fix bug for votes when logged out

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 post '/questions/:question_id/votes' do
+ @question = Question.find(params[:question_id])
   if logged_in?
-    @question = Question.find(params[:question_id])
     @question.votes << Vote.new(vote_value: params[:vote_input].to_i, judge_id: current_user)
     if request.xhr?
       @question.object_votes.to_s
@@ -8,13 +8,17 @@ post '/questions/:question_id/votes' do
       redirect "/questions/#{@question.id}"
     end
   else
-    redirect 'sessions/new'
+    if request.xhr?
+      "#{@question.object_votes}     *Please log-in to vote"
+    else
+      redirect 'sessions/new'
+    end
   end
 end
 
 post '/answers/:answer_id/votes' do
+  @answer = Answer.find(params[:answer_id])
   if logged_in?
-    @answer = Answer.find(params[:answer_id])
     @answer.votes << Vote.new(vote_value: params[:vote_input].to_i, judge_id: current_user)
     if request.xhr?
       @answer.object_votes.to_s
@@ -22,13 +26,17 @@ post '/answers/:answer_id/votes' do
     redirect "/questions/#{@answer.question.id}"
     end
   else
-    redirect 'sessions/new'
+    if request.xhr?
+      "#{@answer.object_votes}     *Please log-in to vote"
+    else
+      redirect 'sessions/new'
+    end
   end
 end
 
 post '/comments/:comment_id/votes' do
+  @comment = Comment.find(params[:comment_id])
   if logged_in?
-    @comment = Comment.find(params[:comment_id])
     @comment.votes << Vote.new(vote_value: params[:vote_input].to_i, judge_id: current_user)
     if request.xhr?
       @comment.object_votes.to_s
@@ -36,7 +44,11 @@ post '/comments/:comment_id/votes' do
     redirect "/#{@comment.commentable_type.downcase}s/#{@comment.commentable.id}/comments"
     end
   else
-    redirect 'sessions/new'
+    if request.xhr?
+      "#{@comment.object_votes}     *Please log-in to vote"
+    else
+      redirect 'sessions/new'
+    end
   end
 end
 
